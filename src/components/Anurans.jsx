76c861a02f4712dd327a90fcd984064aae4dd6f7
Thomas F <thomasfran1935@gmail.com>
2022-02-03 17:48:53 +0100

    MORE CONTENT

--- src/components/Anurans.jsx
+++ src/components/Anurans.jsx
@@ -19,6 +19,50 @@ const items = [
     level: 50,
     content: "Ice Elf Sculpture; Enhanced CP Charm I; Enhanced EXP Charm I; Enhanced Essence Powder Lv50; Fire Elf Sculpture",
   },
+  {
+    level: 55,
+    content: "Enhanced CP Champ II; Enhanced EXP Charm II; Thunder Elf Sculpture; Enhanced Essence Powder Lv55; Nature Elf Sculpture",
+  },
+  {
+    level: 60,
+    content: "Enchanted Gem; Electric Energy Metal; Enhanced Essence Powder Lv60; Darkness Elf Sculpture; Holy Light Sculpture",
+  },
+  {
+    level: 65,
+    content: "Hidden Detection Potion; Enhanced Essence Powder Lv65; All Purpose Potion; Four Seasons Hibernation",
+  },
+  {
+    level: 70,
+    content: "Empty Potent Mp Potion IV Bag; Anuran Energy Drink; Empty HP Potion IV Bag; Frozen In Time Capsule; Enhanced Essence Powder Lv70",
+  },
+  {
+    level: 75,
+    content: "Upgrade Skill reset Scroll V; Baby Phoenix; Eternal Potent MP Potion IV; Eternal Potent HP Potion IV; Enhanced Essence Powder Lv75",
+  },
+  {
+    level: 80,
+    content: "Lavender Perfume; Baby Powder; Royal Bath Salt; Oak Moss Steroil; Lavender Perfume; Teddy Bear Shampoo; Enhanced Essence Powder Lv80",
+  },
+  {
+    level: 85,
+    content: "MP Potion V Bag; HP Potion V Bag; Enhanced EXP Charm III; Enhanced CP Charm III; Enhanced Essence Powder Lv85",
+  },
+  {
+    level: 90,
+    content: "Carnage Potion Bag; Enhanced Essence Powder Lv90; Pet Skill token; HP Little Sprite; MP Little Sprite",
+  },
+  {
+    level: 95,
+    content: "Iron Skin Potion Lv1; Swiftness Potion Lv1; Natures Call Potion Lv1; Ultimate HP Potion Lv1; Ultimate MP Potion Lv1",
+  },
+  {
+    level: 100,
+    content: "Iron Skin Potion Lv2; Swiftness Potion Lv2; Natures Call Potion Lv2; Ultimate HP Potion Lv2; Ultimate MP Potion Lv2",
+  },
+  {
+    level: 100,
+    content: "Green Bean Phoenix; En. Awakening CP Charm I; En. Awakening CP Charm II; En. Awakening CP Charm III; State Removal Potion Lv2",
+  },
 ];
 
 const Anurans = () => {
--- src/components/Zumi.jsx
+++ src/components/Zumi.jsx
@@ -1,4 +1,57 @@
-const items = [];
+const items = [
+  {
+    level: 30,
+    content: "Strike Patch; Destroyer Patch; Sniper Patch; Frozen Patch; Storm Patch",
+  },
+  {
+    level: 40,
+    content: "Invisible Cape; Evasion Cloak; Rocket Boot; Electromagnetic Shield",
+  },
+  {
+    level: 50,
+    content: "Fluctuwave Device; Aven Teleport Device; Asbee Crystal Polymerizer; Eternal Coin Mold; War Stone Polymerizer",
+  },
+  {
+    level: 60,
+    content: "Enhanced Rocket Boot; Enhanced Wave Sensor; Enhanced Invisible Cload; En Electromagnetic Cloak",
+  },
+  {
+    level: 60,
+    content: "Steam Core; Steam Locomotive; Alpaca DNA Mod - Formula; Premium Lucky Safety Stone; Premium Safety Stone",
+  },
+  {
+    level: 70,
+    content: "Super Fertilizer; Firework; Super Speed Device; Wonder Pocket; Avian Amazement",
+  },
+  {
+    level: 70,
+    content: "Eden Crystal Fragment Fuzer; Vitality enhancer; Light Beam Gun; Zumi Flying Propeller; Super Engine Set",
+  },
+  {
+    level: 80,
+    content: "Minibot 8000; Smoke Bomb; Announcement Billboard; Twist Of Fate",
+  },
+  {
+    level: 85,
+    content: "Fourth Dimension Bag; Remote Control Dice; Poisonous Powder; Engineer Potion; Happy Powder",
+  },
+  {
+    level: 90,
+    content: "Mouse Trap; Arch Iron Robot I; General Mechanical Loricae; High Quality Pet Food",
+  },
+  {
+    level: 95,
+    content: "Chance Refill Pouch; Fate Dice; Fate EXP Double Up Scroll; Map reset Tool; Adventure Notebook",
+  },
+  {
+    level: 100,
+    content: "Arch Iron Robot II; Wizard Boots; Warrior Boots; Priest Boots; Guardian Boots",
+  },
+  {
+    level: 100,
+    content: "Basic Fire Stone; Basic Ice Stone; Basic Thunder Stone; Basic Nature Stone; Basic Holy Stone; Basic Dark Stone",
+  },
+];
 
 const Zumis = () => {
   return (
